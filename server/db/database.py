eb7f8e67e6712702afef327ab2e560517fab0836
Hasibul Hasan Chowdhury <hchowdhury404@gmail.com>
2022-08-18 16:54:46 +0600

    Update database.py

--- server/db/database.py
+++ server/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://username:password@localhost:5436/nudges"
+SQLALCHEMY_DATABASE_URL = "postgresql://username:password@db:5432/nudges"
 POOL_SIZE = 20
 POOL_RECYCLE = 3600
 POOL_TIMEOUT = 15
